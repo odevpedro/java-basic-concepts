8f9a7e8a2397ae85eb98182fe092b61311ede2c3
odevpedro <pedrosschmidt2@gmail.com>
2022-07-22 22:58:12 -0300

    segunda instancia

--- src/main/java/CriarConta.java
+++ src/main/java/CriarConta.java
@@ -4,5 +4,21 @@ public class CriarConta {
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
 
+        //adicionando mais saldo na primeira conta
+        primeiraConta.saldo += 200;
+        System.out.println(primeiraConta.saldo);
+
+        //instanciando uma nova conta
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 200;
+        System.out.println(segundaConta.saldo);
+
+        //adicionando mais saldo na primeira conta
+        segundaConta.saldo += 500;
+        System.out.println(segundaConta.saldo);
+
+        System.out.println("saldo da primeira conta" + primeiraConta.saldo);
+        System.out.println("saldo da segunda conta" + segundaConta.saldo);
+
     }
 }
