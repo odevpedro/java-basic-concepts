4852c7ec9951e4e59b040622e123bc80de7fd7f5
odevpedro <pedrosschmidt2@gmail.com>
2023-03-14 18:39:13 -0300

    Using arrays

---
+++ src/main/java/Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //arrays are used to store a list of itens
+
+        String lesson1 = "HTML Course";
+        String lesson2 = "CSS Course";
+        String lesson3 = "JS Course";
+
+        String[] myList = new String[10];
+        myList[0] = lesson1;
+        myList[1] = lesson2;
+        myList[2] = lesson3;
+
+        System.out.println(myList[0]);
+
+    }
+
+
+
+}
--- src/main/java/TypeIndex.java
+++ src/main/java/TypeIndex.java
@@ -58,6 +58,8 @@ public class TypeIndex {
 
 
 
+
+
     }
 
 
