feade38b56b46f250ff4c55043e094bfeb263d37
odevpedro <pedrosschmidt2@gmail.com>
2023-03-14 13:46:10 -0300

    all primitive types

---
+++ src/main/java/Primitives.java
@@ -0,0 +1,22 @@
+public class Primitives {
+    /*In Java, you should use primitives for basic data types such as integers or booleans, and reference types for complex data structures like objects.*/
+    public static void main(String[] args) {
+        byte a = 127; // byte data type
+        short b = 32000; // short data type
+        int c = 2000000000; // integer data type
+        long d = 9223372036854775807L; // long data type
+        float e = 3.141592f; // floating-point data type
+        double f = 3.14159265358979323846; // double floating-point data type
+        boolean g = false; //boolean data type
+        char h = 'a'; // character data type
+
+        System.out.println("byte: " + a);
+        System.out.println("short: " + b);
+        System.out.println("int: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("boolean: " + g);
+        System.out.println("char: " + h);
+    }
+}
