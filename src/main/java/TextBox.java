185f92c95c918e630c08ad46b5627dee9bdaaa67
odevpedro <pedrosschmidt2@gmail.com>
2023-04-18 04:32:59 -0300

    classes

---
+++ src/main/java/TextBox.java
@@ -0,0 +1,12 @@
+class TextBox {
+    public String text;
+
+    public void setText (String text){
+        this.text = text;
+    }
+
+    public void clear(){
+        this.text = "";
+    }
+
+}
