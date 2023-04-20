54af40bc2a8a49c84cad9724a7567d6417d7c2fe
odevpedro <pedrosschmidt2@gmail.com>
2023-04-20 08:56:37 -0300

    metodos e lambdas

--- src/main/java/Stream.java
+++ src/main/java/Stream.java
@@ -1,10 +1,37 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Stream {
     static List<String> funcionarios;
 
-    public static void filtrar(String letra){
-        funcionarios.stream().filter(a -> a.startsWith(letra)).forEach(System.out::print);)
+        //métodos: filtrar ordenar e contar
+
+        public static void filtrar(String letra){
+            funcionarios.stream().filter(a -> a.startsWith(letra)).forEach(System.out::println);
+        }
+
+        public static void ordenar(String letra){
+        funcionarios.stream().sorted().filter(a -> a.startsWith(letra)).forEach(System.out::println);
+        }
+
+        public static void contar(String letra){
+            long contatodor = funcionarios.stream().filter(a -> a.startsWith(letra)).count();
+            System.out.println(contatodor);
+        }
+
+    public static void main(String[] args) {
+        funcionarios = new ArrayList<>();
+        funcionarios.add("Antonio");
+        funcionarios.add("José");
+        funcionarios.add("Pedro");
+        funcionarios.add("João");
+        funcionarios.add("Andréia");
+        filtrar("A");
+        ordenar("A");
+        contar("A");
+
     }
+
+
     
 }
